a6e333b8f4e2cbc5337db6c963afb4ee8802ced0
Walter Hurtado <walter.hurtado@lkmx.io>
2024-12-24 11:46:23 -0800

    industries index homepage

--- src/components/industriesComponent.js
+++ src/components/industriesComponent.js
@@ -3,61 +3,62 @@ import styles from "@/components/industriesComponent.module.scss";
 import { Block, Column } from "@lkmx/flare-react";
 import Image from "next/image";
 import Link from "next/link";
-import getLang from '@/lang';
+import getLang from "@/lang";
 import { useRouter } from "next/router";
 
 export default function IndustriesComponent() {
-    const { locale } = useRouter();
-    const $t = getLang(locale);
-    const industriesCat = $t.industries.catalogItems
+  const { locale } = useRouter();
+  const $t = getLang(locale);
+  const [industries, setIndustries] = useState([]);
 
-    return (
-        <Column className={styles.industries}>
-            <Block className={styles.industries__block}>
-                <h2>{$t.industriesComponent.title}</h2>
-                <p>{$t.industriesComponent.par[0]} <br/>{$t.industriesComponent.par[1]} <br/>{$t.industriesComponent.par[2]}</p>
-                <div className={styles.industries__block__grid}>
-                    {/* <div className={styles.industries__block__grid__firstRow}> */}
-                        {industriesCat.map((data, index)=>{
-                            return(
-                                // index <=4 ?
-                                <div className={styles.industries__block__grid__item} key={index}>
-                                    <div className={styles.industries__block__grid__item__image}>
-                                        <Image
-                                        fill
-                                        src={data.img}
-                                        alt="Industry icon"
-                                        />
-                                    </div>
-                                    {data.title1 ? <span>{data.title1[0]} <br/>{data.title1[1]}</span> : <span>{data.title}</span>}                                    
-                                </div>
-                                // : ''
-                            )
-                        })}
-                    {/* </div> */}
-                    {/* <div className={styles.industries__block__grid__secondRow}>
-                        {industriesCat.map((data, index)=>{
-                            return(
-                                index > 4 ?
-                                <div className={styles.industries__block__grid__secondRow__item} key={index}>
-                                    <div className={styles.industries__block__grid__secondRow__item__image}>
-                                        <Image
-                                        fill
-                                        src={data.img}
-                                        alt="Industry icon"
-                                        />                                        
-                                    </div>
-                                    {data.title1 ? <span>{data.title1[0]} <br/>{data.title1[1]}</span> : <span>{data.title}</span>}    
-                                </div>
-                                : ''
-                            )
-                        })}
-                    </div> */}
+  useEffect(() => {
+    fetch(`/api/footer-data?locale=${locale}`)
+      .then((res) => res.json())
+      .then((data) => setIndustries(data.industries))
+      .catch((error) => console.error("Error:", error));
+  }, [locale]);
+
+  return (
+    <Column className={styles.industries}>
+      <Block className={styles.industriesblock}>
+        <h2>{$t.industriesComponent.title}</h2>
+        <p>
+          {$t.industriesComponent.par[0]} <br />
+          {$t.industriesComponent.par[1]} <br />
+          {$t.industriesComponent.par[2]}
+        </p>
+        <div className={styles.industriesblockgrid}>
+          {Array.isArray(industries) &&
+            industries.map((industry, index) => (
+              <Link
+                href={industry.route}
+                key={index}
+                className={styles.industriescard}
+              >
+                <div className={styles.industriescardcontent}>
+                  <div className={styles.industriescardicon}>
+                    <Image
+                      width={32}
+                      height={32}
+                      src={`/icons/${industry.iconName}.svg`}
+                      alt={industry.title}
+                    />
+                  </div>
+                  <h3>{industry.title}</h3>
+                  <p>{industry.description || ""}</p>
+                  <div className={styles.industriescardarrow}>
+                    <Image
+                      width={24}
+                      height={24}
+                      src="/icons/arrow-right--pink.svg"
+                      alt="arrow"
+                    />
+                  </div>
                 </div>
-                <Link href="/industries" legacyBehavior>
-                    <div className={styles.industries__block__btn} >{$t.industriesComponent.btn}</div>
-                </Link>
-            </Block>
-        </Column>
-    );
+              </Link>
+            ))}
+        </div>
+      </Block>
+    </Column>
+  );
 }
